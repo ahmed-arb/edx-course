edd73edb5d3167a939819767ff6b057c81c1dd95
Ahmed Khalid <106074266+ahmed-arb@users.noreply.github.com>
2023-03-30 13:52:36 +0500

    add course list view

--- edx_course/urls.py
+++ edx_course/urls.py
@@ -3,11 +3,10 @@ URLs for edx_course.
 """
 from django.urls import path
 
-from edx_course.views import hello
+from edx_course.views import CourseListAPIView, hello
 
 
 urlpatterns = [
-    path(
-        'hello/', hello, name='hello'
-    ),
+    path('hello/', hello, name='hello'),
+    path('list/', CourseListAPIView.as_view(), name='course_list'),
 ]
--- edx_course/views.py
+++ edx_course/views.py
@@ -3,7 +3,15 @@ views for edx_course.
 """
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.generics import ListAPIView
+from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from lms.djangoapps.course_api.serializers import CourseSerializer
 
 @api_view(['GET'])
 def hello(request):
     return Response({'msg': "hello"})
+
+class CourseListAPIView(ListAPIView):
+
+    queryset = CourseOverview.get_all_courses()
+    serializer = CourseSerializer
